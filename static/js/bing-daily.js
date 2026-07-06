74f75e1046982e54a81b88248e34e2d82af6a6e9
wag <iswangaiguo@163.com>
2026-07-06 11:07:27 +0800

    header 嵌入 Bing 每日一图，半透明遮罩保证文字可读

---
+++ static/js/bing-daily.js
@@ -0,0 +1,19 @@
+(function() {
+  var header = document.querySelector('body>header');
+  if (!header) return;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1');
+  xhr.onload = function() {
+    if (xhr.status >= 200 && xhr.status < 400) {
+      try {
+        var data = JSON.parse(xhr.responseText);
+        if (data.images && data.images.length > 0) {
+          var url = 'https://cn.bing.com' + data.images[0].url;
+          header.style.backgroundImage = 'url(' + url + ')';
+        }
+      } catch (e) {}
+    }
+  };
+  xhr.send();
+})();
